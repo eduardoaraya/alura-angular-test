bdc0f0bc0f5bdbf19a191289bdfb0b11103284f8
Eduardo Felipe Araya <eduardojezine@gmail.com>
2021-12-06 14:29:55 -0300

    Refactor call name of class and methods

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,7 +1,7 @@
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
-  it('#generateUniqueIdWithPrefix shoud generated id when called with prefix', () => {
+describe(UniqueIdService.name, () => {
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} shoud generated id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
     expect(id).toContain('app-');
